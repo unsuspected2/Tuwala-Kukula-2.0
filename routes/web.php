b46ab08443623c08624e2fca8c68959f20a90c25
INFOSI <josaidnascimento@gmail.com>
2024-11-29 10:19:36 +0100

    add jetstreram

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,13 @@ Route::get('/', function () {
     return redirect()->route('visitantes');
 });
 Route::get('/visitantes', [VisitanteController::class, 'index'])->name('visitantes');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
