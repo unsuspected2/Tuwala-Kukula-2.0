90e7d0af34478d0f0776e4411d4365115869437c
josaidn <josaidnascimento@gmail.com>
2025-01-06 10:07:15 +0100

    rota welcome mudada

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return redirect()->route('teste');
+    return redirect()->route('visitantes');
 });
 
 Route::get('/index2', function () {
